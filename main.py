7ef375985daf544ba8f19192332a45031cfbb097
Kenny Peng <kennypeng515@gmail.com>
2023-11-10 14:19:35 -0800

    flesh out dynamo connectivity

--- main.py
+++ main.py
@@ -4,21 +4,92 @@ from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
 from os.path import join, dirname
+from decimal import Decimal
+import json
+import boto3
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 # main driver for the lambda function.
 def handler(event=None, context=None):
+    # example log
+    # logger.info(event)
+
+    # load necessary environment variables
     dotenv_path = join(dirname(__file__), '.env')
     load_dotenv(dotenv_path)
-    print("environment variable test: " + os.environ['MINESWEEPER_USERNAME'])
-    # TODO: some kind of logging
-    # TODO: still need to 1) ingest the SNS event, and get the URL / other info from that, and 2) publish to Dynamoo
-    # depending on solve percentage / existence of something in lambda, maybe don't use results.
+
+    # we assume that the event payload is a JSON string, so we have to de-serialize it into a dictionary to get what we need
+    raw_message = event['Records'][0]['Sns']['Message']
+    message_dict = json.loads(raw_message)
+    game_url, game_timestamp, failsafe = ""
+    try:
+        game_url = message_dict["game-url"]
+        game_timestamp = message_dict["game-timestamp"]
+        failsafe = message_dict["failsafe"]
+    except:
+        # log
+        raise
+
+    # validate the failsafe
+    if failsafe != os.environ['PERSONAL_SALT']:
+        # log
+        raise
+
+    # connect to Dynamo
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table(os.environ['DYNAMO_NAME'])
+
+    # parse out the ID from the URL
+    game_id = game_url.split('/')[-1]
+
+    # check if the ID corresponding to the URL passed in has an existing entry in Dynamo already.
+    # if it does, simply return.
+    matching_id = table.get_item(Key={"game-id": game_id})
+    if "Item" in matching_id:
+        # log
+        print("already exists, returning.")
+        return
+
+    # process the event
+    result_block, difficulty_selector = scrape_minesweeper_online_game(game_url)
+    statistics, difficulty = process_scraped_minesweeper_game(result_block, difficulty_selector, os.environ['MINESWEEPER_USERNAME'])
+
+    # check to see if this is meaningful (i.e., the game wasn't too short)
+    if statistics["solve-percentage"] < 25.0:
+        # log
+        return
+
+    # now, actually write to Dynamo
+    table.put_item(
+        Item={
+            "game-id": game_id,
+            "game-timestamp": game_timestamp,
+            "difficulty": difficulty,
+            "elapsed-time": Decimal(str(statistics["elapsed-time"])),
+            "estimated-time": Decimal(str(statistics["estimated-time"])),
+            "board-solved": statistics["board-solved"],
+            "completed-3bv": statistics["completed-3bv"],
+            "board-3bv": statistics["board-3bv"],
+            "game-3bvps": Decimal(str(statistics["game-3bvps"])),
+            "useful-clicks": statistics["useful-clicks"],
+            "wasted-clicks": statistics["wasted-clicks"],
+            "total-clicks": statistics["total-clicks"],
+            "efficiency": Decimal(str(statistics["efficiency"])),
+            "solve-percentage": Decimal(str(statistics["solve-percentage"]))
+        }
+    )
+    # log
+    return
+
 
 # given a URL, visits and scrapes for minesweeper game statistics.
 # if no statistics are found, an exception is raised.
 # returns a result block string, and html for the difficulty selector.
 def scrape_minesweeper_online_game(url):
     # if run locally, this configuration block is neded:
+    '''
     options = webdriver.ChromeOptions()
     service = webdriver.ChromeService("/usr/local/bin/chromedriver")
     options.add_argument("--headless=new")
@@ -28,12 +99,11 @@ def scrape_minesweeper_online_game(url):
     options.add_argument("--disable-dev-tools")
     options.add_argument("--no-zygote")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
+    '''
 
     # in run in lambda, this configuration block is needed
-    '''
     options = webdriver.ChromeOptions()
     service = webdriver.ChromeService("/opt/chromedriver")
-
     options.binary_location = '/opt/chrome/chrome'
     options.add_argument("--headless=new")
     options.add_argument('--no-sandbox')
@@ -48,11 +118,9 @@ def scrape_minesweeper_online_game(url):
     options.add_argument(f"--disk-cache-dir={mkdtemp()}")
     options.add_argument("--remote-debugging-port=9222")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36")
-    '''
     
     driver = webdriver.Chrome(options=options, service=service)
     driver.get(url)
-    # https://www.selenium.dev/documentation/webdriver/waits/
     # wait at MOST x seconds for rendering; if things found before then, return right away.
     # this is intentionally kept fairly low, to reduce the possibility of lambda taking forever ($$$)
     # TODO: local tests have shown 7 seconds is more than enough, but running this in lambda may show otherwise.
@@ -89,9 +157,8 @@ def scrape_minesweeper_online_game(url):
         # difficulty_selector = driver.find_element(By.CLASS_NAME, "btn btn-sm btn-default btn-level-select dropdown-toggle")
         difficulty_selector_html = difficulty_selector.get_attribute("innerHTML")
 
-        # quit driver
+        # quit and return
         driver.quit()
-
         return (result_block_text, difficulty_selector_html)
     except Exception as e:
         driver.quit()
@@ -153,7 +220,7 @@ def process_scraped_minesweeper_game(result_block_text, difficulty_selector_html
     efficiency_value = -1.0 if not efficiency_string else float(efficiency_string.strip().strip('%'))
 
     # calculate solved percentage
-    solve_percentage_value = 100.0 if board_solved else float(completed_3bv_value)/float(board_3bv_value)
+    solve_percentage_value = 100.0 if board_solved else (float(completed_3bv_value)/float(board_3bv_value)) * 100.0
 
     # create return objects and return
     statistics = {
@@ -170,11 +237,3 @@ def process_scraped_minesweeper_game(result_block_text, difficulty_selector_html
         "solve-percentage": solve_percentage_value
     }
     return (statistics, difficulty)
-    
-
-# local testing:
-dotenv_path = join(dirname(__file__), '.env')
-load_dotenv(dotenv_path)
-scrape = scrape_minesweeper_online_game("https://minesweeper.online/game/1986919801")
-result = process_scraped_minesweeper_game(scrape[0], scrape[1], os.environ['MINESWEEPER_USERNAME'])
-print(result)
